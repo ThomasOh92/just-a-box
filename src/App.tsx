e61e77ca2ae76658825abf5ea403313aa836e948
Thomas Oh <ohcsthomas@gmail.com>
2024-03-04 18:27:50 +0000

    scaffolding

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import ProjectBox from './components/dashboard';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/dashboard.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+const { useState } = React;
+
+
+const Dashboard: React.FC = () => {
+
+  
+  // Render begins here
+  return (
+    <>
+    Hello
+    </>
+  );
+  
+};
+
+export default Dashboard;
